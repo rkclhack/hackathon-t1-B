ff11e60e777b1638b686f0771bde49a4965550f9
kouki-m <koki.m221030@gmail.com>
2025-07-31 13:05:06 +0900

    fix: onMounted

--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject, ref } from "vue"
+import { inject, ref, onMounted } from "vue"
 import { useRouter } from "vue-router"
 import socketManager from '../socketManager.js'
 
